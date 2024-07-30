36c526f3c686edab3f6544a3ded370f30a961254
230500226 <125464717+230500226@users.noreply.github.com>
2024-07-30 19:54:59 +0200

    init netbeans

---
+++ JavaEx2/src/main/java/com/mycompany/javaex2/JavaEx2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javaex2;
+
+/**
+ *
+ * @author ldxt460s
+ */
+public class JavaEx2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
